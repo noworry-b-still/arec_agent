f9accedc478ab0266742ec85fc299a25c321fd3d
noworry-b-still <teslaatozinspires7@gmail.com>
2025-11-29 16:43:49 -0800

    make real network call for async

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,52 @@
-use tokio::io::Join;
-use tokio::task::JoinHandle;
-use tokio::time::{Duration, sleep};
+use serde::Deserialize;
 
-async fn fetch_research_data(id: u8) -> String {
-    let delay = Duration::from_secs(2);
-    println!("Task {} started: Fetching data...", id);
-    sleep(delay).await;
-    println!("Task {} finished: Data retrieved.", id);
-    format!("Result from Task: {}", id)
+#[derive(Debug, Deserialize)]
+struct ResearchItem {
+    #[serde(rename = "userId")]
+    user_id: u32,
+    id: u32,
+    title: String,
+    #[serde(rename = "completed")]
+    is_completed: bool,
 }
 
+use anyhow::Result;
+
+async fn fetch_and_parse_data(item_id: u32) -> Result<ResearchItem> {
+    println!("-> Task {}: Initiating request.", item_id);
+
+    let url = format!("https://jsonplaceholder.typicode.com/todos/{}", item_id);
+
+    // 1. Make a Get request
+    let response = reqwest::get(&url).await?;
+    
+    // 2 use .json()
+    let item = response.json::<ResearchItem>().await?;
+    println!("<- Task {}: Data received and parsed.", item_id);
+
+    Ok(item)
+}
 #[tokio::main]
-async fn main() {
-    println!("A-ReC Agent Starting Concurrent Fetch...");
+async fn main() -> Result<()>{
+    println!("\n🚀 A-ReC Agent Day 2: Real I/O Observation Demo...\n");
 
-    // 1. create a vector to store joinHandles
-    let mut handles: Vec<JoinHandle<String>> = Vec::new();
+    let mut handles = Vec::new();
 
-    // 2. spawn 3 tasks concurrently
     for i in 1..=3 {
-        let handle = tokio::spawn(fetch_research_data(i));
+        let handle = tokio::spawn(fetch_and_parse_data(i));
         handles.push(handle);
     }
+    println!("Main thread awaiting results...\n");
 
-    // 3. wait for all spawned tasks to complete sequentially
-    println!("\nMain thread is now awaiting the results of the concurrent tasks...");
-
-    let mut results: Vec<String> = Vec::new();
     for handle in handles {
-        let result = handle.await.unwrap();
-        results.push(result);
+        match handle.await? {
+            Ok(item) => println!(
+                "[SUCCESS] Item {}: Title='{}', Completed={}",
+                item.id, item.title, item.is_completed
+            ),
+            Err(e) => eprintln!("[ERROR] Failed to fetch item: {}", e),
+        }
     }
-
-    println!("\n✅ All concurrent tasks completed.");
-    println!("Final Results: {:?}", results);
-    println!("A-ReC Agent Shutting Down.");
+    println!("\n✅ All concurrent tasks processed.");
+    Ok(())
 }
